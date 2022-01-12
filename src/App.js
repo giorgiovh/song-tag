6823b62e52917a5b3a7b6178c47092c5e0f76b43
giorgiovh <jorgevonhoroch@gmail.com>
2022-01-12 16:01:48 -0500

    renders person

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React, { Component } from "react";
 import './App.css';
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Song Tag</h1>
-    </div>
-  );
+    this.state = {
+      person: "Giorgio",
+      letters: "H or T",
+      daysLeft: 2
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Person: {this.state.person}</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
