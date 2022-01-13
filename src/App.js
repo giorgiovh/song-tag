eebd2ff41325abe5eef61a19cae923b407df78a3
giorgiovh <jorgevonhoroch@gmail.com>
2022-01-12 19:57:18 -0500

    renames index variable to personIdx

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import './App.css';
 import Person from './components/Person'
 
 const persons = ["Giorgio", "Aditya", "Kevin", "Hamza", "Alex"]
+const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      index: 0,
+      personIdx: 0,
       letters: "H or T",
       daysLeft: 2
     }
   }
 
   increaseIndex = () => {
-    this.setState({index: this.state.index + 1})
+    this.setState({personIdx: this.state.personIdx + 1})
   }
 
   render() {
     return (
       <>
         {/* <Person person={persons[this.state.index]}></Person> */}
-        <Person person={persons[this.state.index % persons.length]}></Person>
+        <Person person={persons[this.state.personIdx % persons.length]}></Person>
         <button onClick={this.increaseIndex}>Submit Song</button>
       </>
     )
