647f2cd272ba443b52a709bb466a12c38318a7a2
giorgiovh <jorgevonhoroch@gmail.com>
2022-01-12 19:39:47 -0500

    adds button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import './App.css';
 import Person from './components/Person'
+
+const persons = ["Giorgio", "Aditya", "Kevin", "Hamza", "Alex"]
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      person: "Giorgio",
+      index: 0,
       letters: "H or T",
       daysLeft: 2
     }
   }
 
+  increaseIndex = () => {
+    this.setState({index: this.state.index + 1})
+  }
+
   render() {
     return (
-      <Person person={this.state.person}></Person>
+      <>
+        <Person person={persons[this.state.index]}></Person>
+        <button onClick={this.increaseIndex}>Submit Song</button>
+      </>
     )
   }
 }
