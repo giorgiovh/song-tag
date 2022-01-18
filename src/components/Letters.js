12675e45aacd4b9d5fb8b270f1581dd9e5955ef3
giorgiovh <jorgevonhoroch@gmail.com>
2022-01-18 11:56:29 -0500

    formats the 'choose from' message

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -1,6 +1,6 @@
 const Letters = (props) => {
     return (
-        <h2>Choose From: {props.aplhLetter} or </h2>
+        <h2>Choose From: {props.aplhLetter} <small>(alphabet letter)</small> or <small>(last letter of previous song)</small></h2>
     )
 }
 
